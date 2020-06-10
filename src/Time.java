215c4ac0c8c3c728d0c8e864a65abf0c6c652df4
Acer <vika_kotova111@list.ru>
2020-06-10 18:04:42 +0300

    --

--- src/Time.java
+++ src/Time.java
@@ -10,32 +10,41 @@ public class Time<lublic> {
     public String show() {
         String hours = Integer.toString(h);
         String minute = Integer.toString(m);
-        if (h < 10) { hours = "0" + hours;}
-        if (m < 10) { minute = "0" + minute;}
+        if (h < 10) {
+            hours = "0" + hours;
+        }
+        if (m < 10) {
+            minute = "0" + minute;
+        }
         return hours + ":" + minute;
     }
 
     public boolean isMorning() {
-        return( h >= 6 && h < 12);
+        return (h >= 6 && h < 12);
     }
 
     public boolean isDay() {
-        return( h >= 12 && h < 18);
+        return (h >= 12 && h < 18);
     }
 
     public boolean isEvening() {
-        return( h >= 18 && h < 24);
+        return (h >= 18 && h < 24);
     }
 
     public boolean isNight() {
-        return( h >= 0 && h < 6);
+        return (h >= 0 && h < 6);
     }
 
     public String sayHello() {
-        if (isMorning()) { return  "Доброе утро!";}
-        else if (isDay()) {return "Добрый день!";}
-        else if (isEvening()) {return "Добрый вечер!";}
-        else {return  "Доброй ночи!";}
+        if (isMorning()) {
+            return "Доброе утро!";
+        } else if (isDay()) {
+            return "Добрый день!";
+        } else if (isEvening()) {
+            return "Добрый вечер!";
+        } else {
+            return "Доброй ночи!";
+        }
     }
 
     public String add(int n) {
@@ -43,9 +52,11 @@ public class Time<lublic> {
         if (m > 60) {
             m = m % 60;
             h++;
-            if (h > 23) { h = 0; }
+            if (h > 23) {
+                h = 0;
+            }
         }
-        return show() ;
+        return show();
     }
 
 }
