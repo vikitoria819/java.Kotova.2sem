6ce3b15b7413f88057459e55f01e70ce4d114c98
Acer <vika_kotova111@list.ru>
2020-06-10 17:59:03 +0300

    --

--- src/ExperimentsWithTime.java
+++ src/ExperimentsWithTime.java
@@ -1,6 +1,6 @@
 public class ExperimentsWithTime {
-    public static void main(String[] args){
-        Time s = new Time(12,40);
+    public static void main(String[] args) {
+        Time s = new Time(12, 40);
         System.out.println(s.show());
         System.out.println(s.isMorning());
         System.out.println(s.isDay());
