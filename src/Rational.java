871e8e4f2cbc1ac10139c2d99dc4555840afb0a1
Acer <vika_kotova111@list.ru>
2020-06-10 18:02:41 +0300

    --

--- src/Rational.java
+++ src/Rational.java
@@ -6,8 +6,8 @@ public class Rational {
     public static final Rational ONE = new Rational(1);
 
     public Rational(int n, int d) {
-        this.n = n/gcd(n,d);
-        this.d = d/gcd(n,d);
+        this.n = n / gcd(n, d);
+        this.d = d / gcd(n, d);
     }
 
     public Rational(int n) {
@@ -16,17 +16,19 @@ public class Rational {
     }
 
     public String toString() {
-        if (d == 1) { return Integer.toString(n);}
+        if (d == 1) {
+            return Integer.toString(n);
+        }
         return n + "/" + d;
     }
 
     public double toDouble() {
-        return (double)n/d;
+        return (double) n / d;
     }
 
     public static int gcd(int a, int b) {
-        while (b !=0) {
-            int neutral = a%b;
+        while (b != 0) {
+            int neutral = a % b;
             a = b;
             b = neutral;
         }
@@ -36,39 +38,41 @@ public class Rational {
     public Rational add(Rational r2) {
         this.n = this.n * r2.d + r2.n * this.d;
         this.d = this.d * r2.d;
-        int k = gcd(this.n,this.d);
-        this.n = this.n/k;
-        this.d = this.d/k;
-        return new Rational(this.n,this.d);
+        int k = gcd(this.n, this.d);
+        this.n = this.n / k;
+        this.d = this.d / k;
+        return new Rational(this.n, this.d);
     }
 
     public Rational sub(Rational r2) {
         this.n = this.n * r2.d - r2.n * this.d;
         this.d = this.d * r2.d;
-        int k = gcd(this.n,this.d);
-        this.n = this.n/k;
-        this.d = this.d/k;
-        return new Rational(this.n,this.d);
+        int k = gcd(this.n, this.d);
+        this.n = this.n / k;
+        this.d = this.d / k;
+        return new Rational(this.n, this.d);
     }
 
     public Rational mul(Rational r2) {
         this.n = this.n * r2.n;
         this.d = this.d * r2.d;
-        int k = gcd(this.n,this.d);
-        this.n = this.n/k;
-        this.d = this.d/k;
-        return new Rational(this.n,this.d);
+        int k = gcd(this.n, this.d);
+        this.n = this.n / k;
+        this.d = this.d / k;
+        return new Rational(this.n, this.d);
     }
 
     public Rational div(Rational r2) {
         this.n = this.n * r2.d;
         if (this.d != 0 && r2.n != 0) {
             this.d = this.d * r2.n;
-        } else { System.out.println("На 0 делить нельзя!");}
-        int k = gcd(this.n,this.d);
-        this.n = this.n/k;
-        this.d = this.d/k;
-        return new Rational(this.n,this.d);
+        } else {
+            System.out.println("На 0 делить нельзя!");
+        }
+        int k = gcd(this.n, this.d);
+        this.n = this.n / k;
+        this.d = this.d / k;
+        return new Rational(this.n, this.d);
     }
 
     static Rational add(Rational r1, Rational r2) {
@@ -87,11 +91,11 @@ public class Rational {
         return r1.div(r2);
     }
 
-    public int getNumerator(){
+    public int getNumerator() {
         return n;
     }
 
-    public int getDenominator(){
-        return  d;
+    public int getDenominator() {
+        return d;
     }
 }
