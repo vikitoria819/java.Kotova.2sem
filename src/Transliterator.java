71adb19a307594f0a6771c2aa3b5205925d86641
Acer <vika_kotova111@list.ru>
2020-06-20 14:59:15 +0300

    Добавлено 2.2

--- src/Transliterator.java
+++ src/Transliterator.java
@@ -7,6 +7,12 @@ public class Transliterator {
         this.out = eng;
     }
 
+    public static Transliterator createICAO_DOC_9303() {
+        char[] rus =  {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+        String[] eng = {"a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "i", "k", "l", "m", "n", "o", "p", "r","s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "ie", "y", "", "e", "iu", "ia"};
+        return new Transliterator(rus, eng);
+    }
+
 
     public String translateOneSymbol(char c) {
         int k = in.length;
