20ae8226fe6bc6164fcb9655bf8e7a3a18e1fc1a
Acer <vika_kotova111@list.ru>
2020-06-20 21:10:49 +0300

    исправление замечаний

--- src/Transliterator.java
+++ src/Transliterator.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -14,8 +15,8 @@ public class Transliterator {
     }
 
     public static Transliterator createICAO_DOC_9303() {
-        char[] rus =  {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
-        String[] eng = {"a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "i", "k", "l", "m", "n", "o", "p", "r","s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "ie", "y", "", "e", "iu", "ia"};
+        char[] rus = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+        String[] eng = {"a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "i", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "ie", "y", "", "e", "iu", "ia"};
         return new Transliterator(rus, eng);
     }
 
@@ -23,7 +24,7 @@ public class Transliterator {
     public String translateOneSymbol(char c) {
         int k = in.length;
         for (int i = 0; i < k; i++) {
-            if ( in[i] == Character.toLowerCase(c)) {
+            if (in[i] == Character.toLowerCase(c)) {
                 if (Character.isUpperCase(c)) {
                     if (out[i].length() > 0) {
                         return out[i].substring(0, 1).toUpperCase() + out[i].substring(1);
@@ -46,15 +47,11 @@ public class Transliterator {
         return res;
     }
 
-    public void translateFile( String s1, String s2) throws IOException {
-        try {
-            PrintStream ps1 = new PrintStream(new File(s2), "utf8");
-            String content = Files.readString(Paths.get(s1));
-            ps1.println(translate(content));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void translateFile(String s1, String s2) throws IOException {
+        PrintStream ps1 = new PrintStream(new File(s2), StandardCharsets.UTF_8);
+        String content = Files.readString(Paths.get(s1));
+        ps1.println(translate(content));
+        ps1.close();
     }
 
 }
